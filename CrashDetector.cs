a39d140e1a159d37f29b15d4d4363e1f10d2076e
judithwu <judithwu@umich.edu>
2022-12-25 20:24:10 -0800

    finished

---
+++ CrashDetector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float delay = 2f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Ground" && !hasCrashed) {
+            hasCrashed = true;
+            FindObjectOfType<PlayerController>().DisableControls();
+            crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            Invoke("ReloadScene", delay);
+        }
+    }
+
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
+    }
+}
